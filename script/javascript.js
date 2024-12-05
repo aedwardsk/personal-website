6a9a7aa4f4df5a242bc2fd33cd4f87d3ba6e4127
Andy Edwards <andyedwards@outlook.com>
2024-12-05 10:48:14 -0500

    Added Js sucess

--- script/javascript.js
+++ script/javascript.js
@@ -1,4 +1,10 @@
 // Create string
 const hello = "Hello World";
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
 
-alert("Hello World");
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
