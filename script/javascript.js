6110df7a7f56d1fc785425b3fba9532c04eef937
Andy Edwards <andyedwards@outlook.com>
2024-12-05 12:24:41 -0500

    debuged js files

--- script/javascript.js
+++ script/javascript.js
@@ -6,5 +6,5 @@ const submitButton = document.querySelector("#submit");
 form.addEventListener("submit", (e) => {
   submitButton.disabled = true;
   e.preventDefault();
-  window.location.href = window.location.origin + "/success.html";
+  window.location.href = window.location.origin + "/html/success.html";
 });
